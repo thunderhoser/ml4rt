4e9f92c33c149854deeb384e7776bbf7327caa9d
thunderhoser <lagerqui@ualberta.ca>
2020-07-07 10:00:03 -0600

    Added script run_gradcam.py.

--- ml4rt/machine_learning/gradcam.py
+++ ml4rt/machine_learning/gradcam.py
@@ -1,11 +1,26 @@
 """Helper methods for Grad-CAM (gradient-weighted class-activation maps)."""
 
 import numpy
+import netCDF4
 from keras import backend as K
+from gewittergefahr.gg_utils import file_system_utils
 from gewittergefahr.gg_utils import error_checking
 from gewittergefahr.deep_learning import gradcam as gradcam_utils
 
 DEFAULT_IDEAL_ACTIVATION = 2.
+
+MODEL_FILE_KEY = 'model_file_name'
+ACTIVATION_LAYER_KEY = 'activation_layer_name'
+VECTOR_OUT_LAYER_KEY = 'vector_output_layer_name'
+OUTPUT_NEURONS_KEY = 'output_neuron_indices'
+IDEAL_ACTIVATION_KEY = 'ideal_activation'
+
+EXAMPLE_DIMENSION_KEY = 'example'
+HEIGHT_DIMENSION_KEY = 'height'
+EXAMPLE_ID_CHAR_DIM_KEY = 'example_id_char'
+
+EXAMPLE_IDS_KEY = 'example_id_strings'
+CLASS_ACTIVATIONS_KEY = 'class_activation_matrix'
 
 
 def _normalize_tensor(input_tensor):
@@ -17,6 +32,34 @@ def _normalize_tensor(input_tensor):
 
     rms_tensor = K.sqrt(K.mean(K.square(input_tensor)))
     return input_tensor / (rms_tensor + K.epsilon())
+
+
+def check_metadata(
+        activation_layer_name, vector_output_layer_name, output_neuron_indices,
+        ideal_activation):
+    """Checks metadata for errors.
+
+    :param activation_layer_name: Name of activation layer.
+    :param vector_output_layer_name: Name of layer that outputs predictions for
+        vector target variables.
+    :param output_neuron_indices: length-2 numpy array with indices of output
+        neuron (height index, channel index).  Class activation will be computed
+        with respect to the output of this neuron.
+    :param ideal_activation: Ideal neuron activation, used to define loss
+        function.  The loss function will be
+        (output_neuron_activation - ideal_activation)**2.
+    """
+
+    error_checking.assert_is_string(activation_layer_name)
+    error_checking.assert_is_string(vector_output_layer_name)
+
+    error_checking.assert_is_integer_numpy_array(output_neuron_indices)
+    error_checking.assert_is_geq_numpy_array(output_neuron_indices, 0)
+    error_checking.assert_is_numpy_array(
+        output_neuron_indices, exact_dimensions=numpy.array([2], dtype=int)
+    )
+
+    error_checking.assert_is_not_nan(ideal_activation)
 
 
 def run_gradcam(
@@ -31,16 +74,11 @@ def run_gradcam(
     :param model_object: Trained model (instance of `keras.models.Model` or
         `keras.models.Sequential`).
     :param predictor_matrix: H-by-P numpy array of predictor values.
-    :param activation_layer_name: Name of activation layer.
-    :param vector_output_layer_name: Name of layer that outputs predictions for
-        vector target variables.
-    :param output_neuron_indices: length-2 numpy array with indices of output
-        neuron (height index, then channel index).  Class activation will be
-        computed with respect to the output of this neuron.
-    :param ideal_activation: Ideal neuron activation, used to define loss
-        function.  The loss function will be
-        (output_neuron_activation - ideal_activation)**2.
-    :return: layer_activation_matrix: length-H numpy array of class activations.
+    :param activation_layer_name: See doc for `check_metadata`.
+    :param vector_output_layer_name: Same.
+    :param output_neuron_indices: Same.
+    :param ideal_activation: same.
+    :return: class_activations: length-H numpy array of class activations.
     """
 
     # Check input args.
@@ -48,13 +86,12 @@ def run_gradcam(
     error_checking.assert_is_numpy_array(predictor_matrix, num_dimensions=2)
     predictor_matrix = numpy.expand_dims(predictor_matrix, axis=0)
 
-    error_checking.assert_is_integer_numpy_array(output_neuron_indices)
-    error_checking.assert_is_geq_numpy_array(output_neuron_indices, 0)
-    error_checking.assert_is_numpy_array(
-        output_neuron_indices, exact_dimensions=numpy.array([2], dtype=int)
+    check_metadata(
+        activation_layer_name=activation_layer_name,
+        vector_output_layer_name=vector_output_layer_name,
+        output_neuron_indices=output_neuron_indices,
+        ideal_activation=ideal_activation
     )
-
-    error_checking.assert_is_not_nan(ideal_activation)
 
     # Set up loss function.
     output_tensor = model_object.get_layer(
@@ -84,18 +121,151 @@ def run_gradcam(
 
     # Compute class-activation map in activation layer's space.
     mean_weight_by_filter = numpy.mean(gradient_matrix, axis=0)
-    class_activation_matrix = numpy.ones(layer_activation_matrix.shape[:-1])
+    class_activations = numpy.ones(layer_activation_matrix.shape[0])
     num_filters = len(mean_weight_by_filter)
 
     for k in range(num_filters):
-        class_activation_matrix += (
-            mean_weight_by_filter[k] * layer_activation_matrix[..., k]
+        class_activations += (
+            mean_weight_by_filter[k] * layer_activation_matrix[:, k]
         )
 
     num_input_heights = predictor_matrix.shape[1]
     class_activation_matrix = gradcam_utils._upsample_cam(
-        class_activation_matrix=class_activation_matrix,
+        class_activation_matrix=class_activations,
         new_dimensions=numpy.array([num_input_heights], dtype=int)
     )
 
     return numpy.maximum(class_activation_matrix, 0.)
+
+
+def write_standard_file(
+        netcdf_file_name, class_activation_matrix, example_id_strings,
+        model_file_name, activation_layer_name, vector_output_layer_name,
+        output_neuron_indices, ideal_activation):
+    """Writes standard (non-averaged) class-activation maps to NetCDF file.
+
+    E = number of examples
+    H = number of heights
+
+    :param netcdf_file_name: Path to output file.
+    :param class_activation_matrix: E-by-H numpy array of class activations.
+    :param example_id_strings: length-E list of example IDs.
+    :param model_file_name: Path to file with neural net used to create saliency
+        maps (readable by `neural_net.read_model`).
+    :param activation_layer_name: See doc for `check_metadata`.
+    :param vector_output_layer_name: Same.
+    :param output_neuron_indices: Same.
+    :param ideal_activation: Same.
+    """
+
+    # Check input args.
+    check_metadata(
+        activation_layer_name=activation_layer_name,
+        vector_output_layer_name=vector_output_layer_name,
+        output_neuron_indices=output_neuron_indices,
+        ideal_activation=ideal_activation
+    )
+
+    error_checking.assert_is_string(model_file_name)
+
+    error_checking.assert_is_string_list(example_id_strings)
+    error_checking.assert_is_numpy_array(
+        numpy.array(example_id_strings), num_dimensions=1
+    )
+
+    error_checking.assert_is_geq_numpy_array(class_activation_matrix, 0.)
+    error_checking.assert_is_numpy_array(
+        class_activation_matrix, num_dimensions=2
+    )
+
+    num_examples = len(example_id_strings)
+    num_heights = class_activation_matrix.shape[1]
+    expected_dim = numpy.array([num_examples, num_heights], dtype=int)
+    error_checking.assert_is_numpy_array(
+        class_activation_matrix, exact_dimensions=expected_dim
+    )
+
+    # Write to NetCDF file.
+    file_system_utils.mkdir_recursive_if_necessary(file_name=netcdf_file_name)
+    dataset_object = netCDF4.Dataset(
+        netcdf_file_name, 'w', format='NETCDF3_64BIT_OFFSET'
+    )
+
+    dataset_object.setncattr(MODEL_FILE_KEY, model_file_name)
+    dataset_object.setncattr(ACTIVATION_LAYER_KEY, activation_layer_name)
+    dataset_object.setncattr(VECTOR_OUT_LAYER_KEY, vector_output_layer_name)
+    dataset_object.setncattr(OUTPUT_NEURONS_KEY, output_neuron_indices)
+    dataset_object.setncattr(IDEAL_ACTIVATION_KEY, ideal_activation)
+
+    dataset_object.createDimension(EXAMPLE_DIMENSION_KEY, num_examples)
+    dataset_object.createDimension(HEIGHT_DIMENSION_KEY, num_heights)
+
+    if num_examples == 0:
+        num_id_characters = 1
+    else:
+        num_id_characters = numpy.max(numpy.array([
+            len(id) for id in example_id_strings
+        ]))
+
+    dataset_object.createDimension(EXAMPLE_ID_CHAR_DIM_KEY, num_id_characters)
+
+    this_string_format = 'S{0:d}'.format(num_id_characters)
+    example_ids_char_array = netCDF4.stringtochar(numpy.array(
+        example_id_strings, dtype=this_string_format
+    ))
+
+    dataset_object.createVariable(
+        EXAMPLE_IDS_KEY, datatype='S1',
+        dimensions=(EXAMPLE_DIMENSION_KEY, EXAMPLE_ID_CHAR_DIM_KEY)
+    )
+    dataset_object.variables[EXAMPLE_IDS_KEY][:] = numpy.array(
+        example_ids_char_array
+    )
+
+    dataset_object.createVariable(
+        CLASS_ACTIVATIONS_KEY, datatype=numpy.float32,
+        dimensions=(EXAMPLE_DIMENSION_KEY, HEIGHT_DIMENSION_KEY)
+    )
+    dataset_object.variables[CLASS_ACTIVATIONS_KEY][:] = (
+        class_activation_matrix
+    )
+
+    dataset_object.close()
+
+
+def read_standard_file(netcdf_file_name):
+    """Reads standard (non-averaged) class-activation maps from NetCDF file.
+
+    :param netcdf_file_name: Path to input file.
+    :return: gradcam_dict: Dictionary with the following keys.
+    gradcam_dict['class_activation_matrix']: See doc for `write_standard_file`.
+    gradcam_dict['example_id_strings']: Same.
+    gradcam_dict['model_file_name']: Same.
+    gradcam_dict['activation_layer_name']: Same.
+    gradcam_dict['vector_output_layer_name']: Same.
+    gradcam_dict['output_neuron_indices']: Same.
+    gradcam_dict['ideal_activation']: Same.
+    """
+
+    dataset_object = netCDF4.Dataset(netcdf_file_name)
+
+    gradcam_dict = {
+        CLASS_ACTIVATIONS_KEY:
+            dataset_object.variables[CLASS_ACTIVATIONS_KEY][:],
+        EXAMPLE_IDS_KEY: [
+            str(id) for id in
+            netCDF4.chartostring(dataset_object.variables[EXAMPLE_IDS_KEY][:])
+        ],
+        MODEL_FILE_KEY: str(getattr(dataset_object, MODEL_FILE_KEY)),
+        ACTIVATION_LAYER_KEY:
+            str(getattr(dataset_object, ACTIVATION_LAYER_KEY)),
+        VECTOR_OUT_LAYER_KEY:
+            str(getattr(dataset_object, VECTOR_OUT_LAYER_KEY)),
+        OUTPUT_NEURONS_KEY: numpy.array(
+            getattr(dataset_object, OUTPUT_NEURONS_KEY), dtype=int
+        ),
+        IDEAL_ACTIVATION_KEY: getattr(dataset_object, IDEAL_ACTIVATION_KEY)
+    }
+
+    dataset_object.close()
+    return gradcam_dict
---
+++ ml4rt/scripts/run_gradcam.py
@@ -0,0 +1,220 @@
+"""Runs the Grad-CAM (gradient-weighted class-activation maps) algorithm."""
+
+import os.path
+import argparse
+import numpy
+from gewittergefahr.gg_utils import time_conversion
+from ml4rt.io import example_io
+from ml4rt.utils import misc as misc_utils
+from ml4rt.machine_learning import gradcam
+from ml4rt.machine_learning import neural_net
+
+SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
+
+MODEL_FILE_ARG_NAME = 'input_model_file_name'
+EXAMPLE_FILE_ARG_NAME = 'input_example_file_name'
+EXAMPLE_INDICES_ARG_NAME = 'example_indices'
+NUM_EXAMPLES_ARG_NAME = 'num_examples'
+ACTIVATION_LAYER_ARG_NAME = 'activation_layer_name'
+VECTOR_OUT_LAYER_ARG_NAME = 'vector_output_layer_name'
+NEURON_INDICES_ARG_NAME = 'output_neuron_indices'
+IDEAL_ACTIVATION_ARG_NAME = 'ideal_activation'
+OUTPUT_FILE_ARG_NAME = 'output_gradcam_file_name'
+
+MODEL_FILE_HELP_STRING = (
+    'Path to trained model.  Will be read by `neural_net.read_model`.'
+)
+EXAMPLE_FILE_HELP_STRING = (
+    'Path to file with data examples.  Will be read by `example_io.read_file`.'
+)
+EXAMPLE_INDICES_HELP_STRING = (
+    'Indices of examples to use.  If you do not want to use specific examples, '
+    'leave this alone.'
+)
+NUM_EXAMPLES_HELP_STRING = (
+    '[used only if `{0:s}` is not specified] Number of examples to use (these '
+    'will be selected randomly).  If you want to use all examples, leave this '
+    'alone.'
+).format(EXAMPLE_INDICES_ARG_NAME)
+
+ACTIVATION_LAYER_HELP_STRING = 'See doc for `gradcam.check_metadata`.'
+VECTOR_OUT_LAYER_HELP_STRING = 'See doc for `gradcam.check_metadata`.'
+NEURON_INDICES_HELP_STRING = 'See doc for `gradcam.check_metadata`.'
+IDEAL_ACTIVATION_HELP_STRING = 'See doc for `gradcam.check_metadata`.'
+OUTPUT_FILE_HELP_STRING = (
+    'Path to output file.  Will be written by `gradcam.write_standard_file`.'
+)
+
+INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + MODEL_FILE_ARG_NAME, type=str, required=True,
+    help=MODEL_FILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + EXAMPLE_FILE_ARG_NAME, type=str, required=True,
+    help=EXAMPLE_FILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + EXAMPLE_INDICES_ARG_NAME, type=int, nargs='+', required=False,
+    default=[-1], help=EXAMPLE_INDICES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_EXAMPLES_ARG_NAME, type=int, required=False, default=-1,
+    help=NUM_EXAMPLES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + ACTIVATION_LAYER_ARG_NAME, type=str, required=True,
+    help=ACTIVATION_LAYER_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + VECTOR_OUT_LAYER_ARG_NAME, type=str, required=True,
+    help=VECTOR_OUT_LAYER_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NEURON_INDICES_ARG_NAME, type=int, nargs=2, required=True,
+    help=NEURON_INDICES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + IDEAL_ACTIVATION_ARG_NAME, type=float, required=False,
+    default=gradcam.DEFAULT_IDEAL_ACTIVATION, help=IDEAL_ACTIVATION_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_FILE_ARG_NAME, type=str, required=True,
+    help=OUTPUT_FILE_HELP_STRING
+)
+
+
+def _run(model_file_name, example_file_name, example_indices, num_examples,
+         activation_layer_name, vector_output_layer_name, output_neuron_indices,
+         ideal_activation, output_file_name):
+    """Runs the Grad-CAM (gradient-weighted class-activation maps) algorithm.
+
+    This is effectively the main method.
+
+    :param model_file_name: See documentation at top of file.
+    :param example_file_name: Same.
+    :param example_indices: Same.
+    :param num_examples: Same.
+    :param activation_layer_name: Same.
+    :param vector_output_layer_name: Same.
+    :param output_neuron_indices: Same.
+    :param ideal_activation: Same.
+    :param output_file_name: Same.
+    :raises: ValueError: if neural-net type is not CNN or U-net.
+    """
+
+    this_example_dict = example_io.read_file(example_file_name)
+    num_examples_total = len(this_example_dict[example_io.VALID_TIMES_KEY])
+
+    example_indices = misc_utils.subset_examples(
+        indices_to_keep=example_indices, num_examples_to_keep=num_examples,
+        num_examples_total=num_examples_total
+    )
+
+    print('Reading model from: "{0:s}"...'.format(model_file_name))
+    model_object = neural_net.read_model(model_file_name)
+
+    metafile_name = neural_net.find_metafile(
+        model_dir_name=os.path.split(model_file_name)[0],
+        raise_error_if_missing=True
+    )
+
+    print('Reading metadata from: "{0:s}"...'.format(metafile_name))
+    metadata_dict = neural_net.read_metafile(metafile_name)
+
+    net_type_string = metadata_dict[neural_net.NET_TYPE_KEY]
+    valid_net_type_strings = [
+        neural_net.CNN_TYPE_STRING, neural_net.U_NET_TYPE_STRING
+    ]
+
+    if net_type_string not in valid_net_type_strings:
+        error_string = (
+            '\nThis script does not work for net type "{0:s}".  Works only for '
+            'those listed below:\n{1:s}'
+        ).format(net_type_string, str(valid_net_type_strings))
+
+        raise ValueError(error_string)
+
+    year = example_io.file_name_to_year(example_file_name)
+    first_time_unix_sec, last_time_unix_sec = (
+        time_conversion.first_and_last_times_in_year(year)
+    )
+
+    generator_option_dict = metadata_dict[neural_net.TRAINING_OPTIONS_KEY]
+    generator_option_dict[neural_net.EXAMPLE_DIRECTORY_KEY] = (
+        os.path.split(example_file_name)[0]
+    )
+    generator_option_dict[neural_net.BATCH_SIZE_KEY] = num_examples_total
+    generator_option_dict[neural_net.FIRST_TIME_KEY] = first_time_unix_sec
+    generator_option_dict[neural_net.LAST_TIME_KEY] = last_time_unix_sec
+
+    generator = neural_net.data_generator(
+        option_dict=generator_option_dict, for_inference=True,
+        net_type_string=metadata_dict[neural_net.NET_TYPE_KEY],
+        is_loss_constrained_mse=False
+    )
+
+    print(SEPARATOR_STRING)
+    predictor_matrix, _, example_id_strings = next(generator)
+    print(SEPARATOR_STRING)
+
+    predictor_matrix = predictor_matrix[example_indices, ...]
+    example_id_strings = [example_id_strings[i] for i in example_indices]
+
+    num_examples = predictor_matrix.shape[0]
+    num_heights = predictor_matrix.shape[1]
+    class_activation_matrix = numpy.full((num_examples, num_heights), numpy.nan)
+
+    for i in range(num_examples):
+        if numpy.mod(i, 10) == 0:
+            print('Have run Grad-CAM for {0:d} of {1:d} examples...'.format(
+                i, num_examples
+            ))
+
+        class_activation_matrix[i, :] = gradcam.run_gradcam(
+            model_object=model_object, predictor_matrix=predictor_matrix,
+            activation_layer_name=activation_layer_name,
+            vector_output_layer_name=vector_output_layer_name,
+            output_neuron_indices=output_neuron_indices,
+            ideal_activation=ideal_activation
+        )
+
+    print('Have run Grad-CAM for all {0:d} examples!\n'.format(num_examples))
+
+    print('Writing class-activation maps to: "{0:s}"...'.format(
+        output_file_name
+    ))
+    gradcam.write_standard_file(
+        netcdf_file_name=output_file_name,
+        class_activation_matrix=class_activation_matrix,
+        example_id_strings=example_id_strings,
+        model_file_name=model_file_name,
+        activation_layer_name=activation_layer_name,
+        vector_output_layer_name=vector_output_layer_name,
+        output_neuron_indices=output_neuron_indices,
+        ideal_activation=ideal_activation
+    )
+
+
+if __name__ == '__main__':
+    INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
+
+    _run(
+        model_file_name=getattr(INPUT_ARG_OBJECT, MODEL_FILE_ARG_NAME),
+        example_file_name=getattr(INPUT_ARG_OBJECT, EXAMPLE_FILE_ARG_NAME),
+        example_indices=numpy.array(
+            getattr(INPUT_ARG_OBJECT, EXAMPLE_INDICES_ARG_NAME), dtype=int
+        ),
+        num_examples=getattr(INPUT_ARG_OBJECT, NUM_EXAMPLES_ARG_NAME),
+        activation_layer_name=getattr(
+            INPUT_ARG_OBJECT, ACTIVATION_LAYER_ARG_NAME
+        ),
+        vector_output_layer_name=getattr(
+            INPUT_ARG_OBJECT, VECTOR_OUT_LAYER_ARG_NAME
+        ),
+        output_neuron_indices=numpy.array(
+            getattr(INPUT_ARG_OBJECT, NEURON_INDICES_ARG_NAME), dtype=int
+        ),
+        ideal_activation=getattr(INPUT_ARG_OBJECT, IDEAL_ACTIVATION_ARG_NAME),
+        output_file_name=getattr(INPUT_ARG_OBJECT, OUTPUT_FILE_ARG_NAME)
+    )
