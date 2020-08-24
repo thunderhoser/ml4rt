af9a5b347ae8123308887c2f28e682173f72299d
thunderhoser <lagerqui@ualberta.ca>
2020-08-24 16:59:59 -0600

    Added script make_u_net_templates_exp4.py.

--- ml4rt/make_u_net_templates_exp4.py
+++ ml4rt/make_u_net_templates_exp4.py
@@ -1,9 +1,10 @@
-"""Makes U-net templates for Experiment 3."""
+"""Makes U-net templates for Experiment 4."""
 
 import sys
 import copy
 import os.path
 import numpy
+import keras.losses
 
 THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
     os.path.join(os.getcwd(), os.path.expanduser(__file__))
@@ -13,6 +14,7 @@ sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 import u_net_architecture
 import architecture_utils
 import custom_losses
+import normalization
 import file_system_utils
 import neural_net
 
@@ -24,27 +26,29 @@ SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 #     HOME_DIR_NAME = os.path.expanduser('~')
 
 HOME_DIR_NAME = '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist'
-OUTPUT_DIR_NAME = '{0:s}/ml4rt_models/experiment03/templates'.format(
+OUTPUT_DIR_NAME = '{0:s}/ml4rt_models/experiment04/templates'.format(
     HOME_DIR_NAME
 )
 
-CONV_LAYER_DROPOUT_RATES = numpy.array([-1, 0.05, 0.1, 0.15, 0.2, 0.25])
-UPCONV_LAYER_DROPOUT_RATES = numpy.array([
-    -1, 0.05, 0.1, 0.15, 0.2, 0.25
-])
-SKIP_LAYER_DROPOUT_RATES = numpy.array([-1, 0.05, 0.1, 0.15, 0.2, 0.25])
+DENSE_LAYER_COUNTS = numpy.array([2, 3, 4, 5], dtype=int)
+DENSE_LAYER_DROPOUT_RATES = numpy.array([0.125, 0.25, 0.375, 0.5, 0.625, 0.75])
+SCALAR_LOSS_FUNCTION_WEIGHTS = numpy.array([1, 2.5, 5, 10, 25, 50])
 
 NUM_LEVELS = 4
-LOSS_FUNCTION = custom_losses.dual_weighted_mse()
+VECTOR_LOSS_FUNCTION = custom_losses.dual_weighted_mse()
 
 DEFAULT_OPTION_DICT = {
     u_net_architecture.NUM_LEVELS_KEY: NUM_LEVELS,
     u_net_architecture.NUM_HEIGHTS_KEY: 73,
     u_net_architecture.NUM_HEIGHTS_FOR_LOSS_KEY: 73,
     u_net_architecture.NUM_INPUT_CHANNELS_KEY: 16,
+    u_net_architecture.CONV_LAYER_DROPOUT_RATES_KEY:
+        numpy.full(NUM_LEVELS + 1, -1.),
+    u_net_architecture.UPCONV_LAYER_DROPOUT_RATES_KEY:
+        numpy.full(NUM_LEVELS, -1.),
+    u_net_architecture.SKIP_LAYER_DROPOUT_RATES_KEY:
+        numpy.full(NUM_LEVELS, -1.),
     u_net_architecture.OUTPUT_LAYER_DROPOUT_RATES_KEY: numpy.full(2, numpy.nan),
-    u_net_architecture.DENSE_LAYER_NEURON_NUMS_KEY: None,
-    u_net_architecture.DENSE_LAYER_DROPOUT_RATES_KEY: None,
     u_net_architecture.INNER_ACTIV_FUNCTION_KEY:
         architecture_utils.RELU_FUNCTION_STRING,
     u_net_architecture.INNER_ACTIV_FUNCTION_ALPHA_KEY: 0.2,
@@ -58,11 +62,14 @@ DEFAULT_OPTION_DICT = {
     u_net_architecture.HEATING_RATE_INDEX_KEY: None
 }
 
-DUMMY_GENERATOR_OPTION_DICT = {neural_net.VECTOR_TARGET_NORM_TYPE_KEY: None}
+DUMMY_GENERATOR_OPTION_DICT = {
+    neural_net.VECTOR_TARGET_NORM_TYPE_KEY: None,
+    neural_net.SCALAR_TARGET_NORM_TYPE_KEY: normalization.Z_SCORE_NORM_STRING
+}
 
 
 def _run():
-    """Makes U-net templates for Experiment 3.
+    """Makes U-net templates for Experiment 4.
 
     This is effectively the main method.
     """
@@ -71,41 +78,58 @@ def _run():
         directory_name=OUTPUT_DIR_NAME
     )
 
-    num_conv_dropout_rates = len(CONV_LAYER_DROPOUT_RATES)
-    num_upconv_dropout_rates = len(UPCONV_LAYER_DROPOUT_RATES)
-    num_skip_dropout_rates = len(SKIP_LAYER_DROPOUT_RATES)
+    num_dense_layer_counts = len(DENSE_LAYER_COUNTS)
+    num_dropout_rates = len(DENSE_LAYER_DROPOUT_RATES)
+    num_loss_function_weights = len(SCALAR_LOSS_FUNCTION_WEIGHTS)
 
-    for i in range(num_conv_dropout_rates):
-        for j in range(num_upconv_dropout_rates):
-            for k in range(num_skip_dropout_rates):
+    for i in range(num_dense_layer_counts):
+        these_neuron_counts = architecture_utils.get_dense_layer_dimensions(
+            num_input_units=4096, num_classes=2,
+            num_dense_layers=DENSE_LAYER_COUNTS[i], for_classification=False
+        )
+
+        for j in range(num_dropout_rates):
+            these_dropout_rates = numpy.full(
+                DENSE_LAYER_COUNTS[i], DENSE_LAYER_DROPOUT_RATES[j]
+            )
+            these_dropout_rates[-1] = numpy.nan
+
+            for k in range(num_loss_function_weights):
                 this_option_dict = copy.deepcopy(DEFAULT_OPTION_DICT)
 
                 this_option_dict[
-                    u_net_architecture.CONV_LAYER_DROPOUT_RATES_KEY
-                ] = numpy.full(NUM_LEVELS + 1, CONV_LAYER_DROPOUT_RATES[i])
+                    u_net_architecture.DENSE_LAYER_NEURON_NUMS_KEY
+                ] = these_neuron_counts
 
                 this_option_dict[
-                    u_net_architecture.UPCONV_LAYER_DROPOUT_RATES_KEY
-                ] = numpy.full(NUM_LEVELS, UPCONV_LAYER_DROPOUT_RATES[j])
-
-                this_option_dict[
-                    u_net_architecture.SKIP_LAYER_DROPOUT_RATES_KEY
-                ] = numpy.full(NUM_LEVELS, SKIP_LAYER_DROPOUT_RATES[k])
+                    u_net_architecture.DENSE_LAYER_DROPOUT_RATES_KEY
+                ] = these_dropout_rates
 
                 print(this_option_dict)
                 print(SEPARATOR_STRING)
 
+                this_scalar_loss_function = (
+                    SCALAR_LOSS_FUNCTION_WEIGHTS[k] * keras.losses.mse
+                )
+                this_loss_dict = {
+                    'conv_output': VECTOR_LOSS_FUNCTION,
+                    'dense_output': this_scalar_loss_function
+                }
+
                 this_model_object = u_net_architecture.create_model(
                     option_dict=this_option_dict,
-                    vector_loss_function=LOSS_FUNCTION, num_output_channels=1
+                    vector_loss_function=VECTOR_LOSS_FUNCTION,
+                    num_output_channels=1,
+                    scalar_loss_function=this_scalar_loss_function
                 )
 
                 this_model_file_name = (
-                    '{0:s}/model_conv-dropout={1:.2f}_upconv-dropout={2:.2f}_'
-                    'skip-dropout={3:.2f}.h5'
+                    '{0:s}/model_num-dense-layers={1:d}_dense-dropout={2:.3f}_'
+                    'scalar-lf-weight={3:05.1f}.h5'
                 ).format(
-                    OUTPUT_DIR_NAME, CONV_LAYER_DROPOUT_RATES[i],
-                    UPCONV_LAYER_DROPOUT_RATES[j], SKIP_LAYER_DROPOUT_RATES[k]
+                    OUTPUT_DIR_NAME, DENSE_LAYER_COUNTS[i],
+                    DENSE_LAYER_DROPOUT_RATES[j],
+                    SCALAR_LOSS_FUNCTION_WEIGHTS[k]
                 )
 
                 print('Writing model to: "{0:s}"...'.format(
@@ -131,7 +155,7 @@ def _run():
                     num_validation_batches_per_epoch=100,
                     validation_option_dict=DUMMY_GENERATOR_OPTION_DICT,
                     net_type_string=neural_net.U_NET_TYPE_STRING,
-                    loss_function_or_dict=LOSS_FUNCTION,
+                    loss_function_or_dict=this_loss_dict,
                     do_early_stopping=True, plateau_lr_multiplier=0.6
                 )
 
