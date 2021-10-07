41308202e556e8b860b7ab3948629eb7e276ec6b
thunderhoser <lagerqui@ualberta.ca>
2021-10-07 14:16:48 -0600

    Added files to run RRTM in Docker container.

--- ml4rt/rrtm/docker_laptop/run_rrtm_one_day_interp-heights-but-diff.sh
+++ ml4rt/rrtm/docker_laptop/run_rrtm_one_day_interp-heights-but-diff.sh
@@ -17,7 +17,7 @@ TOP_DATA_DIR_NAME_ACTUAL="/home/ralager/condo/swatwork/ralager/scratch1/RDARCH/r
 TOP_DATA_DIR_NAME_DOCKER="/home/user/data"
 
 cp -v "${GITHUB_CODE_DIR_NAME}/make_rrtm_sw_calc.pro" "${TOP_DATA_DIR_NAME_ACTUAL}/"
-cp -v "${GITHUB_CODE_DIR_NAME}/runit_ML_dataset_builder_745heights.pro" "${TOP_DATA_DIR_NAME_ACTUAL}/"
+cp -v "${GITHUB_CODE_DIR_NAME}/runit_ML_dataset_builder_orig-heights.pro" "${TOP_DATA_DIR_NAME_ACTUAL}/"
 
 actual_gfs_file_name="${TOP_DATA_DIR_NAME_ACTUAL}/profiler_sitesF.ncep_rap.${date_string}.0000.nc"
 echo "GFS file for date ${date_string}: ${actual_gfs_file_name}"
@@ -34,7 +34,7 @@ gdl_script_file_name="${data_dir_name_actual}/run_rrtm_${date_string}.gdl"
 echo "Writing GDL batch script to: '${gdl_script_file_name}'..."
 
 echo ".compile /home/user/data/make_rrtm_sw_calc" > $gdl_script_file_name
-echo ".run /home/user/data/runit_ML_dataset_builder_745heights.pro" >> $gdl_script_file_name
+echo ".run /home/user/data/runit_ML_dataset_builder_orig-heights.pro" >> $gdl_script_file_name
 echo "runit,${year_string}" >> $gdl_script_file_name
 echo "exit" >> $gdl_script_file_name
 
--- ml4rt/rrtm/docker_laptop/run_rrtm_one_day_orig-heights.sh
+++ ml4rt/rrtm/docker_laptop/run_rrtm_one_day_orig-heights.sh
@@ -13,11 +13,11 @@ year_string=${date_string:0:4}
 SEPARATOR_STRING="\r\n--------------------------------------------------\r\n"
 
 GITHUB_CODE_DIR_NAME="/home/ralager/ml4rt/ml4rt/rrtm"
-TOP_DATA_DIR_NAME_ACTUAL="/home/ralager/condo/swatwork/ralager/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4rt_project/gfs_data/new_heights"
+TOP_DATA_DIR_NAME_ACTUAL="/home/ralager/condo/swatwork/ralager/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4rt_project/gfs_data/orig_heights"
 TOP_DATA_DIR_NAME_DOCKER="/home/user/data"
 
 cp -v "${GITHUB_CODE_DIR_NAME}/make_rrtm_sw_calc.pro" "${TOP_DATA_DIR_NAME_ACTUAL}/"
-cp -v "${GITHUB_CODE_DIR_NAME}/runit_ML_dataset_builder_745heights.pro" "${TOP_DATA_DIR_NAME_ACTUAL}/"
+cp -v "${GITHUB_CODE_DIR_NAME}/runit_ML_dataset_builder_orig-heights.pro" "${TOP_DATA_DIR_NAME_ACTUAL}/"
 
 actual_gfs_file_name="${TOP_DATA_DIR_NAME_ACTUAL}/profiler_sitesF.ncep_rap.${date_string}.0000.nc"
 echo "GFS file for date ${date_string}: ${actual_gfs_file_name}"
@@ -34,8 +34,8 @@ gdl_script_file_name="${data_dir_name_actual}/run_rrtm_${date_string}.gdl"
 echo "Writing GDL batch script to: '${gdl_script_file_name}'..."
 
 echo ".compile /home/user/data/make_rrtm_sw_calc" > $gdl_script_file_name
-echo ".run /home/user/data/runit_ML_dataset_builder_745heights.pro" >> $gdl_script_file_name
+echo ".run /home/user/data/runit_ML_dataset_builder_orig-heights.pro" >> $gdl_script_file_name
 echo "runit,${year_string}" >> $gdl_script_file_name
 echo "exit" >> $gdl_script_file_name
 
-echo ${sudo_password} | sudo -S -k docker exec ${container_id_string} /bin/sh -c "cd ${data_dir_name_docker}; ls -lrt ${data_dir_name_docker}; gdl -e '@run_rrtm_${date_string}.gdl'"
+echo ${sudo_password} | sudo -S -k docker exec ${container_id_string} /bin/sh -c "cd ${data_dir_name_docker}; gdl -e '@run_rrtm_${date_string}.gdl'"
