95b31b508931611b36d867513967da1805668aec
thunderhoser <lagerqui@ualberta.ca>
2021-10-21 14:16:03 -0600

    FOO

--- ml4rt/apply_neural_net_with_interp.py
+++ ml4rt/apply_neural_net_with_interp.py
@@ -28,8 +28,8 @@ NUM_EXAMPLES_PER_BATCH = 500
 EXAMPLE_MATCHING_TIME_SEC = 180
 
 MODEL_FILE_ARG_NAME = 'input_model_file_name'
-ORIG_EXAMPLE_DIR_ARG_NAME = 'input_orig_example_dir_name'
-NEW_EXAMPLE_DIR_ARG_NAME = 'input_new_example_dir_name'
+ORIG_EXAMPLE_DIR_ARG_NAME = 'input_orig_unnorm_example_dir_name'
+NEW_EXAMPLE_DIR_ARG_NAME = 'input_new_unnorm_example_dir_name'
 NEW_NORM_FILE_ARG_NAME = 'input_new_norm_file_name'
 HALF_WINDOW_SIZE_ARG_NAME = 'half_window_size_for_interp_px'
 FIRST_TIME_ARG_NAME = 'first_time_string'
@@ -40,12 +40,14 @@ MODEL_FILE_HELP_STRING = (
     'Path to trained model.  Will be read by `neural_net.read_model`.'
 )
 ORIG_EXAMPLE_DIR_HELP_STRING = (
-    'Name of directory with data examples on original grid.  Files therein will'
-    ' be found by `example_io.find_file` and read by `example_io.read_file`.'
+    'Name of directory with unnormalized data examples on original grid.  '
+    'Files therein will be found by `example_io.find_file` and read by '
+    '`example_io.read_file`.'
 )
 NEW_EXAMPLE_DIR_HELP_STRING = (
-    'Name of directory with data examples on new grid.  Files therein will'
-    ' be found by `example_io.find_file` and read by `example_io.read_file`.'
+    'Name of directory with unnormalized data examples on new grid.  Files '
+    'therein will be found by `example_io.find_file` and read by '
+    '`example_io.read_file`.'
 )
 NEW_NORM_FILE_HELP_STRING = (
     'Path to normalization file for new grid.  Will be read by '
@@ -274,9 +276,15 @@ def _get_data_on_orig_grid(
             numpy.expand_dims(predicted_hr_matrix_k_day01, axis=-1)
     }
 
+    print(numpy.sum(numpy.isnan(predicted_hr_matrix_k_day01)))
+    print(predicted_hr_matrix_k_day01.size)
+
     predicted_hr_matrix_w_m02 = example_utils.heating_rate_to_w_m02(
         example_dict
     )
+
+    print(numpy.sum(numpy.isnan(predicted_hr_matrix_w_m02)))
+    print(predicted_hr_matrix_w_m02.size)
 
     return (
         predicted_hr_matrix_w_m02, scalar_prediction_matrix, example_dict
