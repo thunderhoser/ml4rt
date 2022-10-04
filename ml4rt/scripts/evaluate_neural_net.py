8c7743ea336290e18b332ad1f1b3a274aeba67e8
thunderhoser <lagerqui@ualberta.ca>
2022-10-04 10:55:20 -0600

    Don't know what I changed.

--- ml4rt/scripts/evaluate_neural_net.py
+++ ml4rt/scripts/evaluate_neural_net.py
@@ -71,7 +71,8 @@ def _run(prediction_file_name, num_bootstrap_reps, output_dir_name):
 
     result_table_xarray = evaluation.get_scores_all_variables(
         prediction_file_name=prediction_file_name,
-        num_bootstrap_reps=num_bootstrap_reps, num_reliability_bins=100
+        num_bootstrap_reps=num_bootstrap_reps,
+        num_reliability_bins=100, max_bin_edge_percentile=99.5
     )
     print(SEPARATOR_STRING)
 
