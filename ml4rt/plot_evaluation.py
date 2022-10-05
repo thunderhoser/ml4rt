19315c2cdb463f3089208b708d0d6dfca22de5c6
thunderhoser <lagerqui@ualberta.ca>
2022-10-05 16:07:46 -0600

    FOO

--- ml4rt/plot_evaluation.py
+++ ml4rt/plot_evaluation.py
@@ -247,7 +247,6 @@ def _plot_attributes_diagram(
     t = evaluation_tables_xarray[0]
     is_scalar = target_name in t.coords[evaluation.SCALAR_FIELD_DIM].values
     is_aux = target_name in t.coords[evaluation.AUX_TARGET_FIELD_DIM].values
-    is_vector = target_name in t.coords[evaluation.VECTOR_FIELD_DIM].values
 
     if is_scalar:
         target_indices = numpy.array([
@@ -524,7 +523,7 @@ def _plot_attributes_diagram(
         title_string = 'Attributes diagram for {0:s}'.format(
             TARGET_NAME_TO_VERBOSE[target_name]
         )
-        if is_vector:
+        if height_m_agl is not None:
             title_string += ' at {0:d} m AGL'.format(
                 int(numpy.round(height_m_agl))
             )
@@ -582,7 +581,7 @@ def _plot_attributes_diagram(
             output_dir_name, target_name.replace('_', '-')
         )
 
-        if is_vector:
+        if height_m_agl is not None:
             figure_file_name += '_{0:05d}m-agl'.format(
                 int(numpy.round(height_m_agl))
             )
