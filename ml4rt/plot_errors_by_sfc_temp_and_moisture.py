fae5141f35c85ad530013585250e2f5d91e70af8
thunderhoser <lagerqui@ualberta.ca>
2022-11-08 16:30:03 -0700

    FOO

--- ml4rt/plot_errors_by_sfc_temp_and_moisture.py
+++ ml4rt/plot_errors_by_sfc_temp_and_moisture.py
@@ -706,8 +706,8 @@ def _run(prediction_file_name, num_temperature_bins,
             letter_label = chr(ord(letter_label) + 1)
 
         if 'bias' in STATISTIC_NAMES[k]:
-            max_colour_value = numpy.nanmax(
-                numpy.absolute(metric_matrix)
+            max_colour_value = numpy.nanpercentile(
+                numpy.absolute(metric_matrix), 99.5
             )
             min_colour_value = -1 * max_colour_value
             colour_map_object = BIAS_COLOUR_MAP_OBJECT
@@ -716,11 +716,11 @@ def _run(prediction_file_name, num_temperature_bins,
             metric_matrix[numpy.isinf(metric_matrix)] = numpy.nan
 
             min_colour_value = numpy.nanmin(metric_matrix)
-            max_colour_value = numpy.nanmax(metric_matrix)
+            max_colour_value = numpy.nanpercentile(metric_matrix, 99.5)
             colour_map_object = NUM_EXAMPLES_COLOUR_MAP_OBJECT
         else:
             min_colour_value = numpy.nanmin(metric_matrix)
-            max_colour_value = numpy.nanmax(metric_matrix)
+            max_colour_value = numpy.nanpercentile(metric_matrix, 99.5)
             colour_map_object = MAIN_COLOUR_MAP_OBJECT
 
         colour_norm_object = pyplot.Normalize(
