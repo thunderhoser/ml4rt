615cb524e4ef4ef4815d5db1b0f402433fd075ec
thunderhoser <lagerqui@ualberta.ca>
2022-12-23 15:00:08 -0700

    Added script plot_crps.py.

---
+++ ml4rt/scripts/plot_crps.py
@@ -0,0 +1,132 @@
+"""Plots CRPS (cont ranked prob score) histogram for each target variable."""
+
+import argparse
+import numpy
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot
+from gewittergefahr.gg_utils import file_system_utils
+from ml4rt.utils import uq_evaluation
+from ml4rt.plotting import evaluation_plotting
+
+LINE_COLOUR = numpy.array([217, 95, 2], dtype=float)
+
+LEGEND_BOUNDING_BOX_DICT = {
+    'facecolor': 'white',
+    'alpha': 0.7,
+    'edgecolor': 'black',
+    'linewidth': 1,
+    'boxstyle': 'round'
+}
+
+FIGURE_WIDTH_INCHES = 15
+FIGURE_HEIGHT_INCHES = 15
+FIGURE_RESOLUTION_DPI = 300
+
+INPUT_FILE_ARG_NAME = 'input_file_name'
+OUTPUT_DIR_ARG_NAME = 'output_dir_name'
+
+INPUT_FILE_HELP_STRING = (
+    'Path to input file.  Will be read by `uq_evaluation.read_crps`.'
+)
+OUTPUT_DIR_HELP_STRING = (
+    'Name of output directory.  Figures will be saved here.'
+)
+
+INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + INPUT_FILE_ARG_NAME, type=str, required=True,
+    help=INPUT_FILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
+    help=OUTPUT_DIR_HELP_STRING
+)
+
+
+def _run(input_file_name, output_dir_name):
+    """Plots CRPS (cont ranked prob score) histogram for each target variable.
+
+    This is effectively the main method.
+
+    :param input_file_name: See documentation at top of file.
+    :param output_dir_name: Same.
+    """
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        directory_name=output_dir_name
+    )
+
+    print('Reading data from: "{0:s}"...'.format(input_file_name))
+    result_table_xarray = uq_evaluation.read_crps(input_file_name)
+    t = result_table_xarray
+
+    scalar_target_names = (
+        t.coords[uq_evaluation.SCALAR_FIELD_DIM].values.tolist()
+    )
+    scalar_crps_values = t[uq_evaluation.SCALAR_CRPS_KEY].values
+    aux_target_names = (
+        t.coords[uq_evaluation.AUX_TARGET_FIELD_DIM].values.tolist()
+    )
+
+    if len(aux_target_names) > 1:
+        scalar_target_names += aux_target_names
+        scalar_crps_values = numpy.concatenate(
+            (scalar_crps_values, t[uq_evaluation.AUX_CRPS_KEY].values),
+            axis=0
+        )
+
+    vector_target_names = t.coords[uq_evaluation.VECTOR_FIELD_DIM].values
+
+    for k in range(len(vector_target_names)):
+        figure_object, axes_object = pyplot.subplots(
+            1, 1, figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+        )
+        evaluation_plotting.plot_score_profile(
+            heights_m_agl=t.coords[uq_evaluation.HEIGHT_DIM].values,
+            score_values=t[uq_evaluation.VECTOR_CRPS_KEY].values[k, :],
+            score_name=evaluation_plotting.CRPS_NAME,
+            line_colour=LINE_COLOUR, line_width=4, line_style='solid',
+            use_log_scale=True, axes_object=axes_object,
+            are_axes_new=True
+        )
+
+        axes_object.set_xlabel('CRPS')
+        axes_object.set_title(
+            'CRPS for {0:s}'.format(vector_target_names[k])
+        )
+
+        annotation_string = 'CRPS for {0:s} = {1:.3f}'.format(
+            scalar_target_names[0], scalar_crps_values[0]
+        )
+        for k in range(1, len(scalar_target_names)):
+            annotation_string += '\n{0:s} = {1:.3f}'.format(
+                scalar_target_names[k], scalar_crps_values[k]
+            )
+
+        axes_object.text(
+            0.05, 0.5, annotation_string,
+            fontsize=16, color='k',
+            bbox=LEGEND_BOUNDING_BOX_DICT,
+            horizontalalignment='left', verticalalignment='middle',
+            transform=axes_object.transAxes, zorder=1e10
+        )
+
+        figure_file_name = '{0:s}/crps_{1:s}.jpg'.format(
+            output_dir_name, vector_target_names[k].replace('_', '-')
+        )
+        print('Saving figure to file: "{0:s}"...'.format(figure_file_name))
+        figure_object.savefig(
+            figure_file_name, dpi=FIGURE_RESOLUTION_DPI,
+            pad_inches=0, bbox_inches='tight'
+        )
+        pyplot.close(figure_object)
+
+
+if __name__ == '__main__':
+    INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
+
+    _run(
+        input_file_name=getattr(INPUT_ARG_OBJECT, INPUT_FILE_ARG_NAME),
+        output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
+    )
