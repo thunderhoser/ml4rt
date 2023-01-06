0bfb825fbb4a60048660feccb35288c5dda21b11
thunderhoser <lagerqui@ualberta.ca>
2023-01-06 11:49:30 -0700

    FOO

--- ml4rt/apply_neural_net.py
+++ ml4rt/apply_neural_net.py
@@ -340,7 +340,7 @@ def _run(model_file_name, example_dir_name, first_time_string, last_time_string,
             predictor_matrix=predictor_matrix, use_dropout=False
         )
 
-        while len(vector_prediction_matrix.shape) < 4:
+        while len(vector_prediction_matrix.shape) < 3:
             vector_prediction_matrix = numpy.expand_dims(
                 vector_prediction_matrix, axis=-1
             )
