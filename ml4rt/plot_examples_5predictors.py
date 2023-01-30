9d4b824acf58ad070ccb8a0a07a6f7149e891c37
thunderhoser <lagerqui@ualberta.ca>
2023-01-30 16:10:39 -0700

    FOO

--- ml4rt/plot_examples_5predictors.py
+++ ml4rt/plot_examples_5predictors.py
@@ -44,6 +44,12 @@ PREDICTOR_COLOURS_LISTLIST = [
     [ORANGE_COLOUR, GREEN_COLOUR, BLACK_COLOUR]
 ]
 
+TARGET_NAMES = [
+    example_utils.SHORTWAVE_DOWN_FLUX_NAME,
+    example_utils.SHORTWAVE_UP_FLUX_NAME,
+    example_utils.SHORTWAVE_HEATING_RATE_NAME
+]
+
 LINE_WIDTH = 3
 FIGURE_RESOLUTION_DPI = 300
 
@@ -52,6 +58,7 @@ NUM_EXAMPLES_ARG_NAME = make_saliency_maps.NUM_EXAMPLES_ARG_NAME
 EXAMPLE_DIR_ARG_NAME = make_saliency_maps.EXAMPLE_DIR_ARG_NAME
 EXAMPLE_ID_FILE_ARG_NAME = make_saliency_maps.EXAMPLE_ID_FILE_ARG_NAME
 PLOT_SHORTWAVE_ARG_NAME = 'plot_shortwave'
+PLOT_TARGETS_ARG_NAME = 'plot_targets'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 EXAMPLE_FILE_HELP_STRING = make_saliency_maps.EXAMPLE_FILE_HELP_STRING
@@ -60,6 +67,9 @@ EXAMPLE_DIR_HELP_STRING = make_saliency_maps.EXAMPLE_DIR_HELP_STRING
 EXAMPLE_ID_FILE_HELP_STRING = make_saliency_maps.EXAMPLE_ID_FILE_HELP_STRING
 PLOT_SHORTWAVE_HELP_STRING = (
     'Boolean flag.  If 1 (0), will plot shortwave (longwave) variables.'
+)
+PLOT_TARGETS_HELP_STRING = (
+    'Boolean flag.  If 1 (0), will (not) plot target variables.'
 )
 OUTPUT_DIR_HELP_STRING = (
     'Name of output directory (figures will be saved here).'
@@ -87,18 +97,21 @@ INPUT_ARG_PARSER.add_argument(
     help=PLOT_SHORTWAVE_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
+    '--' + PLOT_TARGETS_ARG_NAME, type=int, required=False, default=0,    help=PLOT_TARGETS_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
     help=OUTPUT_DIR_HELP_STRING
 )
 
 
-def _plot_one_example(
-        example_dict, example_index, plot_shortwave, output_dir_name):
+def _plot_one_example(example_dict, example_index, plot_targets,
+                      output_dir_name):
     """Plots data for one example.
 
     :param example_dict: See doc for `example_io.read_file`.
     :param example_index: Will plot results for example with this array index.
-    :param plot_shortwave: See documentation at top of file.
+    :param plot_targets: Boolean flag.
     :param output_dir_name: Name of output directory.  Figures will be saved
         here.
     """
@@ -155,6 +168,32 @@ def _plot_one_example(
     )
     pyplot.close(figure_object)
 
+    if plot_targets:
+        handle_dict = profile_plotting.plot_targets(
+            example_dict=example_dict, example_index=example_index,
+            for_shortwave=True, use_log_scale=True,
+            line_width=LINE_WIDTH, line_style='solid'
+        )
+        figure_object = handle_dict[profile_plotting.FIGURE_HANDLE_KEY]
+
+        this_file_name = '{0:s}/{1:s}_targets.jpg'.format(
+            output_dir_name, example_id_string.replace('_', '-')
+        )
+        panel_file_names.append(this_file_name)
+
+        print('Saving figure to: "{0:s}"...'.format(this_file_name))
+        figure_object.savefig(
+            this_file_name, dpi=FIGURE_RESOLUTION_DPI, pad_inches=0,
+            bbox_inches='tight'
+        )
+        pyplot.close(figure_object)
+
+    for this_file_name in panel_file_names:
+        imagemagick_utils.trim_whitespace(
+            input_file_name=this_file_name, output_file_name=this_file_name,
+            border_width_pixels=10
+        )
+
     num_panels = len(panel_file_names)
     num_panel_rows = int(numpy.floor(
         numpy.sqrt(num_panels)
@@ -181,8 +220,9 @@ def _plot_one_example(
 
 
 def _run(example_file_name, num_examples, example_dir_name,
-         example_id_file_name, plot_shortwave, output_dir_name):
-    """Plots profiles (all target variables and 4 predictor variables).
+         example_id_file_name, plot_shortwave, plot_targets,
+         output_dir_name):
+    """Plots profiles (5 predictor variables).
 
     This is effectively the main method.
 
@@ -218,7 +258,7 @@ def _run(example_file_name, num_examples, example_dir_name,
     for i in range(num_examples):
         _plot_one_example(
             example_dict=example_dict, example_index=i,
-            plot_shortwave=plot_shortwave, output_dir_name=output_dir_name
+            plot_targets=plot_targets, output_dir_name=output_dir_name
         )
 
         print(MINOR_SEPARATOR_STRING)
@@ -235,5 +275,6 @@ if __name__ == '__main__':
             INPUT_ARG_OBJECT, EXAMPLE_ID_FILE_ARG_NAME
         ),
         plot_shortwave=bool(getattr(INPUT_ARG_OBJECT, PLOT_SHORTWAVE_ARG_NAME)),
+        plot_targets=bool(getattr(INPUT_ARG_OBJECT, PLOT_TARGETS_ARG_NAME)),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
     )
