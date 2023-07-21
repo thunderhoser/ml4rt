9c5f0f7d595cdf8871bfec8c9fd78904b5c915ea
thunderhoser <lagerqui@ualberta.ca>
2023-07-21 16:34:57 -0600

    FOO

--- ml4rt/make_templates_2022paper_sw_plusplus_best_1output.py
+++ ml4rt/make_templates_2022paper_sw_plusplus_best_1output.py
@@ -103,10 +103,16 @@ def _run():
                         these_channel_counts_all,
                     u_net_pp_architecture.ENCODER_DROPOUT_RATES_KEY:
                         numpy.full(MODEL_DEPTHS[i] + 1, 0.),
+                    u_net_pp_architecture.ENCODER_MC_DROPOUT_FLAGS_KEY:
+                        numpy.full(MODEL_DEPTHS[i] + 1, 0, dtype=bool),
                     u_net_pp_architecture.UPCONV_DROPOUT_RATES_KEY:
                         numpy.full(MODEL_DEPTHS[i], 0.),
+                    u_net_pp_architecture.UPCONV_MC_DROPOUT_FLAGS_KEY:
+                        numpy.full(MODEL_DEPTHS[i], 0, dtype=bool),
                     u_net_pp_architecture.SKIP_DROPOUT_RATES_KEY:
-                        numpy.full(MODEL_DEPTHS[i], 0.)
+                        numpy.full(MODEL_DEPTHS[i], 0.),
+                    u_net_pp_architecture.SKIP_MC_DROPOUT_FLAGS_KEY:
+                        numpy.full(MODEL_DEPTHS[i], 0, dtype=bool)
                 })
 
                 if MODEL_DEPTHS[i] == 3:
