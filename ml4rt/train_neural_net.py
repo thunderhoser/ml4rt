427ac0847b39f77ffbb49c9ad4c8985b82f7109a
thunderhoser <lagerqui@ualberta.ca>
2023-11-16 17:53:40 -0700

    FOO

--- ml4rt/train_neural_net.py
+++ ml4rt/train_neural_net.py
@@ -168,6 +168,9 @@ def _run(net_type_string, training_dir_name, validation_dir_name,
     metadata_dict = neural_net.read_metafile(input_metafile_name)
     loss_function_or_dict = metadata_dict[neural_net.LOSS_FUNCTION_OR_DICT_KEY]
     bnn_architecture_dict = metadata_dict[neural_net.BNN_ARCHITECTURE_KEY]
+    u_net_plusplus_architecture_dict = metadata_dict[
+        neural_net.U_NET_PP_ARCHITECTURE_KEY
+    ]
 
     print(SEPARATOR_STRING)
 
@@ -183,7 +186,8 @@ def _run(net_type_string, training_dir_name, validation_dir_name,
             net_type_string=net_type_string,
             loss_function_or_dict=loss_function_or_dict, do_early_stopping=True,
             plateau_lr_multiplier=plateau_lr_multiplier,
-            bnn_architecture_dict=bnn_architecture_dict
+            bnn_architecture_dict=bnn_architecture_dict,
+            u_net_plusplus_architecture_dict=u_net_plusplus_architecture_dict
         )
     else:
         neural_net.train_model_sans_generator(
@@ -195,7 +199,8 @@ def _run(net_type_string, training_dir_name, validation_dir_name,
             num_training_batches_per_epoch=num_training_batches_per_epoch,
             num_validation_batches_per_epoch=num_validn_batches_per_epoch,
             plateau_lr_multiplier=plateau_lr_multiplier,
-            bnn_architecture_dict=bnn_architecture_dict
+            bnn_architecture_dict=bnn_architecture_dict,
+            u_net_plusplus_architecture_dict=u_net_plusplus_architecture_dict
         )
 
 
