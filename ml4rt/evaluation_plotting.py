86b667ac3bd8428b23708c9abb428d3c16b57710
thunderhoser <lagerqui@ualberta.ca>
2024-05-02 17:28:23 -0600

    FOO

--- ml4rt/evaluation_plotting.py
+++ ml4rt/evaluation_plotting.py
@@ -345,12 +345,8 @@ def plot_inset_histogram(
     inset_axes_object.set_xticks(x_tick_values)
     inset_axes_object.set_xticklabels(x_tick_labels)
 
-    for this_tick_object in inset_axes_object.xaxis.get_major_ticks():
-        this_tick_object.label.set_fontsize(HISTOGRAM_FONT_SIZE)
-        this_tick_object.label.set_rotation('vertical')
-
-    for this_tick_object in inset_axes_object.yaxis.get_major_ticks():
-        this_tick_object.label.set_fontsize(HISTOGRAM_FONT_SIZE)
+    inset_axes_object.tick_params(axis='x', labelsize=HISTOGRAM_FONT_SIZE)
+    inset_axes_object.tick_params(axis='y', labelsize=HISTOGRAM_FONT_SIZE)
 
     inset_axes_object.set_title(
         'Prediction frequency' if has_predictions else 'Observation frequency',
