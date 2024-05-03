8eb9e29f399ef996f6948c2032789f27751e86d0
thunderhoser <lagerqui@ualberta.ca>
2024-05-03 15:17:41 -0600

    FOO

--- ml4rt/make_templates_spectral_exp01_sw_plusplus.py
+++ ml4rt/make_templates_spectral_exp01_sw_plusplus.py
@@ -10,7 +10,7 @@ THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
 ))
 sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 
-import u_net_pp_architecture
+import u_net_pp_architecture as u_net_pp_arch
 import architecture_utils
 import custom_losses
 import file_system_utils
@@ -59,35 +59,42 @@ CONV_LAYER_COUNTS = numpy.array([1, 2, 3, 4], dtype=int)
 FIRST_LAYER_CHANNEL_COUNTS = numpy.array([4, 8, 16, 32, 64, 128], dtype=int)
 
 DEFAULT_OPTION_DICT = {
-    u_net_pp_architecture.INPUT_DIMENSIONS_KEY:
+    u_net_pp_arch.INPUT_DIMENSIONS_KEY:
         numpy.array([127, 26], dtype=int),
-    # u_net_pp_architecture.NUM_LEVELS_KEY: NUM_LEVELS,
-    # u_net_pp_architecture.CONV_LAYER_COUNTS_KEY:
+    # u_net_pp_arch.NUM_LEVELS_KEY: NUM_LEVELS,
+    # u_net_pp_arch.CONV_LAYER_COUNTS_KEY:
     #     numpy.full(NUM_LEVELS + 1, 2, dtype=int),
-    # u_net_pp_architecture.CHANNEL_COUNTS_KEY: numpy.round(
+    # u_net_pp_arch.CHANNEL_COUNTS_KEY: numpy.round(
     #     numpy.logspace(6, 10, num=NUM_LEVELS + 1, base=2.)
     # ).astype(int),
-    # u_net_pp_architecture.ENCODER_DROPOUT_RATES_KEY:
+    # u_net_pp_arch.ENCODER_DROPOUT_RATES_KEY:
     #     numpy.full(NUM_LEVELS + 1, 0.),
-    # u_net_pp_architecture.UPCONV_DROPOUT_RATES_KEY: numpy.full(NUM_LEVELS, 0.),
-    # u_net_pp_architecture.SKIP_DROPOUT_RATES_KEY: numpy.full(NUM_LEVELS, 0.),
-    u_net_pp_architecture.INCLUDE_PENULTIMATE_KEY: False,
-    u_net_pp_architecture.INNER_ACTIV_FUNCTION_KEY:
+    # u_net_pp_arch.UPCONV_DROPOUT_RATES_KEY: numpy.full(NUM_LEVELS, 0.),
+    # u_net_pp_arch.SKIP_DROPOUT_RATES_KEY: numpy.full(NUM_LEVELS, 0.),
+    u_net_pp_arch.INCLUDE_PENULTIMATE_KEY: False,
+    u_net_pp_arch.INNER_ACTIV_FUNCTION_KEY:
         architecture_utils.RELU_FUNCTION_STRING,
-    u_net_pp_architecture.INNER_ACTIV_FUNCTION_ALPHA_KEY: 0.2,
-    u_net_pp_architecture.CONV_OUTPUT_ACTIV_FUNC_KEY:
+    u_net_pp_arch.INNER_ACTIV_FUNCTION_ALPHA_KEY: 0.2,
+    u_net_pp_arch.CONV_OUTPUT_ACTIV_FUNC_KEY:
         architecture_utils.RELU_FUNCTION_STRING,
-    u_net_pp_architecture.CONV_OUTPUT_ACTIV_FUNC_ALPHA_KEY: 0.,
-    u_net_pp_architecture.DENSE_OUTPUT_ACTIV_FUNC_KEY:
+    u_net_pp_arch.CONV_OUTPUT_ACTIV_FUNC_ALPHA_KEY: 0.,
+    u_net_pp_arch.DENSE_OUTPUT_ACTIV_FUNC_KEY:
         architecture_utils.RELU_FUNCTION_STRING,
-    u_net_pp_architecture.DENSE_OUTPUT_ACTIV_FUNC_ALPHA_KEY: 0.,
-    u_net_pp_architecture.L1_WEIGHT_KEY: 0.,
-    u_net_pp_architecture.L2_WEIGHT_KEY: 1e-7,
-    u_net_pp_architecture.USE_BATCH_NORM_KEY: True,
-    # u_net_pp_architecture.DENSE_LAYER_NEURON_NUMS_KEY: DENSE_LAYER_NEURON_COUNTS,
-    u_net_pp_architecture.DENSE_LAYER_DROPOUT_RATES_KEY: numpy.full(4, 0.),
-    u_net_pp_architecture.VECTOR_LOSS_FUNCTION_KEY: VECTOR_LOSS_FUNCTION,
-    u_net_pp_architecture.SCALAR_LOSS_FUNCTION_KEY: SCALAR_LOSS_FUNCTION
+    u_net_pp_arch.DENSE_OUTPUT_ACTIV_FUNC_ALPHA_KEY: 0.,
+    u_net_pp_arch.L1_WEIGHT_KEY: 0.,
+    u_net_pp_arch.L2_WEIGHT_KEY: 1e-7,
+    u_net_pp_arch.USE_BATCH_NORM_KEY: True,
+    # u_net_pp_arch.DENSE_LAYER_NEURON_NUMS_KEY: DENSE_LAYER_NEURON_COUNTS,
+    u_net_pp_arch.DENSE_LAYER_DROPOUT_RATES_KEY: numpy.full(4, 0.),
+    u_net_pp_arch.DENSE_LAYER_MC_DROPOUT_FLAGS_KEY: numpy.full(
+        4, False, dtype=bool
+    ),
+    u_net_pp_arch.NUM_OUTPUT_WAVELENGTHS_KEY: len(BAND_WEIGHTS),
+    u_net_pp_arch.VECTOR_LOSS_FUNCTION_KEY: VECTOR_LOSS_FUNCTION,
+    u_net_pp_arch.SCALAR_LOSS_FUNCTION_KEY: SCALAR_LOSS_FUNCTION,
+    u_net_pp_arch.USE_DEEP_SUPERVISION_KEY: False,
+    u_net_pp_arch.ENSEMBLE_SIZE_KEY: 1,
+    u_net_pp_arch.DO_INLINE_NORMALIZATION_KEY: False
 }
 
 DUMMY_GENERATOR_OPTION_DICT = {
@@ -123,20 +130,29 @@ def _run():
                 )
 
                 this_option_dict.update({
-                    u_net_pp_architecture.NUM_LEVELS_KEY:
-                        MODEL_DEPTHS[i],
-                    u_net_pp_architecture.CONV_LAYER_COUNTS_KEY:
-                        numpy.full(
-                            MODEL_DEPTHS[i] + 1, CONV_LAYER_COUNTS[j], dtype=int
-                        ),
-                    u_net_pp_architecture.CHANNEL_COUNTS_KEY:
-                        these_channel_counts_all,
-                    u_net_pp_architecture.ENCODER_DROPOUT_RATES_KEY:
-                        numpy.full(MODEL_DEPTHS[i] + 1, 0.),
-                    u_net_pp_architecture.UPCONV_DROPOUT_RATES_KEY:
-                        numpy.full(MODEL_DEPTHS[i], 0.),
-                    u_net_pp_architecture.SKIP_DROPOUT_RATES_KEY:
-                        numpy.full(MODEL_DEPTHS[i], 0.)
+                    u_net_pp_arch.NUM_LEVELS_KEY: MODEL_DEPTHS[i],
+                    u_net_pp_arch.CONV_LAYER_COUNTS_KEY: numpy.full(
+                        MODEL_DEPTHS[i] + 1, CONV_LAYER_COUNTS[j], dtype=int
+                    ),
+                    u_net_pp_arch.CHANNEL_COUNTS_KEY: these_channel_counts_all,
+                    u_net_pp_arch.ENCODER_DROPOUT_RATES_KEY: numpy.full(
+                        MODEL_DEPTHS[i] + 1, 0.
+                    ),
+                    u_net_pp_arch.ENCODER_MC_DROPOUT_FLAGS_KEY: numpy.full(
+                        MODEL_DEPTHS[i] + 1, False, dtype=bool
+                    ),
+                    u_net_pp_arch.UPCONV_DROPOUT_RATES_KEY: numpy.full(
+                        MODEL_DEPTHS[i], 0.
+                    ),
+                    u_net_pp_arch.UPCONV_MC_DROPOUT_FLAGS_KEY: numpy.full(
+                        MODEL_DEPTHS[i], False, dtype=bool
+                    ),
+                    u_net_pp_arch.SKIP_DROPOUT_RATES_KEY: numpy.full(
+                        MODEL_DEPTHS[i], 0.
+                    ),
+                    u_net_pp_arch.SKIP_MC_DROPOUT_FLAGS_KEY: numpy.full(
+                        MODEL_DEPTHS[i], False, dtype=bool
+                    )
                 })
 
                 if MODEL_DEPTHS[i] == 3:
@@ -157,13 +173,10 @@ def _run():
                     )[1]
                 )
 
-                this_option_dict[
-                    u_net_pp_architecture.DENSE_LAYER_NEURON_NUMS_KEY
-                ] = these_neuron_counts
-
-                this_model_object = u_net_pp_architecture.create_model(
-                    this_option_dict
+                this_option_dict[u_net_pp_arch.DENSE_LAYER_NEURON_NUMS_KEY] = (
+                    these_neuron_counts
                 )
+                this_model_object = u_net_pp_arch.create_model(this_option_dict)
 
                 this_model_file_name = (
                     '{0:s}/depth={1:d}_num-conv-layers-per-block={2:d}_'
@@ -193,13 +206,12 @@ def _run():
                     raise_error_if_missing=False
                 )
 
-                this_option_dict[
-                    u_net_pp_architecture.VECTOR_LOSS_FUNCTION_KEY
-                ] = VECTOR_LOSS_FUNCTION_STRING
-
-                this_option_dict[
-                    u_net_pp_architecture.SCALAR_LOSS_FUNCTION_KEY
-                ] = SCALAR_LOSS_FUNCTION_STRING
+                this_option_dict[u_net_pp_arch.VECTOR_LOSS_FUNCTION_KEY] = (
+                    VECTOR_LOSS_FUNCTION_STRING
+                )
+                this_option_dict[u_net_pp_arch.SCALAR_LOSS_FUNCTION_KEY] = (
+                    SCALAR_LOSS_FUNCTION_STRING
+                )
 
                 print('Writing metadata to: "{0:s}"...'.format(
                     this_metafile_name
