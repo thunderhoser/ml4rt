041fc66e41805180d6f2031b1dadd0d79264797c
thunderhoser <lagerqui@ualberta.ca>
2024-05-20 20:41:13 -0600

    FOO

--- ml4rt/u_net_pp_architecture.py
+++ ml4rt/u_net_pp_architecture.py
@@ -215,7 +215,7 @@ def create_model(option_dict):
 
         if has_dense_layers:
             flux_mask_input_layer_object = keras.layers.Input(
-                shape=(num_output_wavelengths, dense_layer_neuron_nums[-1]),
+                shape=(num_output_wavelengths, num_dense_output_vars),
                 name=neural_net.FLUX_MASK_KEY
             )
 
@@ -634,7 +634,7 @@ def create_model_1output_layer(option_dict):
 
         if has_dense_layers:
             flux_mask_input_layer_object = keras.layers.Input(
-                shape=(num_output_wavelengths, dense_layer_neuron_nums[-1]),
+                shape=(num_output_wavelengths, num_dense_output_vars),
                 name=neural_net.FLUX_MASK_KEY
             )
 
