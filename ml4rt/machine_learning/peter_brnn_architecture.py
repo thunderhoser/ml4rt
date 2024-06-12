f1f02d420485e7df39e7c7598f4e7dbf1b35d67f
thunderhoser <lagerqui@ualberta.ca>
2024-06-11 18:06:04 -0600

    Added Peter's architecture and shit.

---
+++ ml4rt/machine_learning/peter_brnn_architecture.py
@@ -0,0 +1,170 @@
+"""Methods for building Peter Ukkonen's BRNN architecture.
+
+BRNN = bidirectional recurrent neural network
+
+https://doi.org/10.1029/2021MS002875
+"""
+
+import numpy
+import keras
+import keras.layers
+from keras import Model
+from tensorflow.keras import backend as K
+
+# Assumes scalar predictors in the following order:
+# solar zenith angle (rad), albedo (1), latitude (deg N), longitude (deg E),
+# column LWP (kg m^-2), column IWP (kg m^-2), aerosol SSA (1), aerosol asymm (1)
+SCALAR_PREDICTOR_MAX_VALUES = numpy.array([
+    1.4835263, 0.8722802, 89.910324, 359.8828,
+    2.730448, 8.228799, 1., 0.96304035
+], dtype=numpy.float32)
+
+# Assumes vector predictors in the following order:
+# pressure (Pa), temperature (K), spec humidity (kg kg^-1), LWC (kg m^-3),
+# IWC (kg m^-3), O3 mixing ratio (kg kg^-1), CO2 (ppmv), CH4 (ppmv),
+# N2O (ppmv), aerosol extinction (m^-1), liquid eff rad (m), ice eff rad (m),
+# height thickness (m), pressure thickness (Pa), height (m AGL), dLWP (kg m^-2),
+# dIWP (kg m^-2), dWVP (kg m^-2), uLWP (kg m^-2), uIWP (kg m^-2),
+# uWVP (kg m^-2), relative humidity (unitless)
+VECTOR_PREDICTOR_MAX_VALUES = numpy.array([
+    1.04399836e+05, 3.18863373e+02, 2.22374070e-02, 1.33532193e-03,
+    1.47309888e-03, 1.73750886e-05, 1.15184314e+03, 6.53699684e+00,
+    1.38715994e+00, 9.72749013e-03, 1.70985604e-05, 6.53300012e-05,
+    7.95751719e+04, 3.15813086e+03, 1.78493945e+03, 2.73044801e+00,
+    8.22879887e+00, 7.84459991e+01, 2.73044801e+00, 8.22879887e+00,
+    7.84459991e+01, 1.23876810e+00
+], dtype=numpy.float32)
+
+NUM_HEIGHTS = 127
+
+
+class HeatingRateLayer(keras.layers.Layer):
+    def __init__(self, name=None, hr_units='K d-1', **kwargs):
+        super(HeatingRateLayer, self).__init__(name=name, **kwargs)
+        time_scale = {'K s-1': 1, 'K d-1': 3600 * 24}[hr_units]
+        self.g_cp = numpy.float32(9.8066 / 1004 * time_scale)
+
+    def get_config(self):
+        cfg = super().get_config()
+        return cfg
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, inputs):
+        fluxes, hlpress = inputs
+        netflux = fluxes[..., 0] - fluxes[..., 1]
+        flux_diff = netflux[..., :-1] - netflux[..., 1:]
+        net_press = hlpress[..., :-1, 0] - hlpress[..., 1:, 0]
+        return -self.g_cp * flux_diff / net_press
+
+
+def rnn_sw(
+        nneur=64, lstm=True,
+        activ_last='sigmoid', activ_surface='linear',
+        add_dense=False, add_scalars_to_levels=True, simpler_inputs=True):
+
+    scalar_input_dims = (len(SCALAR_PREDICTOR_MAX_VALUES),)
+    scalar_input_layer_object = keras.layers.Input(
+        shape=scalar_input_dims, name='scalar_predictor_matrix'
+    )
+
+    vector_input_dims = (NUM_HEIGHTS, len(VECTOR_PREDICTOR_MAX_VALUES))
+    vector_input_layer_object = keras.layers.Input(
+        shape=vector_input_dims, name='vector_predictor_matrix'
+    )
+
+    toa_flux_input_dims = (1, 1)
+    toa_flux_input_layer_object = keras.layers.Input(
+        shape=toa_flux_input_dims, name='toa_flux_input_matrix'
+    )
+
+    all_inp = [
+        scalar_input_layer_object,
+        vector_input_layer_object,
+        toa_flux_input_layer_object
+    ]
+    scalar_inp = all_inp[0]
+    lay_inp = all_inp[1]
+    incflux = all_inp[2]
+
+    hl_p = lay_inp[..., :1]
+    albedos = scalar_inp[:, 1:2]
+
+    lay_inp = lay_inp / VECTOR_PREDICTOR_MAX_VALUES
+    scalar_inp = scalar_inp / SCALAR_PREDICTOR_MAX_VALUES
+
+    if simpler_inputs:
+        lay_inp = lay_inp[:, :, :14]
+
+        scalar_inp = keras.layers.Concatenate(axis=-1)([
+            scalar_inp[:, 0:1],
+            scalar_inp[:, 1:2],
+            scalar_inp[:, 6:7],
+            scalar_inp[:, 7:8]
+        ])
+
+    if add_scalars_to_levels:
+        # new_dimensions = (1, scalar_inp.shape[1])
+        # lay_inp2 = keras.layers.Reshape(target_shape=new_dimensions)(scalar_inp)
+        lay_inp2 = keras.layers.RepeatVector(127)(scalar_inp)
+        lay_inp = keras.layers.Concatenate(axis=-1)([lay_inp, lay_inp2])
+
+    mlp_surface_outp = keras.layers.Dense(
+        nneur, activation=activ_surface, name='dense_surface'
+    )(albedos)
+
+    if lstm:
+        mlp_surface_outp2 = keras.layers.Dense(
+            nneur, activation=activ_surface, name='dense_surface2'
+        )(albedos)
+
+        init_state = [mlp_surface_outp, mlp_surface_outp2]
+        rnnlayer = keras.layers.LSTM
+    else:
+        init_state = mlp_surface_outp
+        rnnlayer = keras.layers.GRU
+
+    if add_dense:
+        lay_inp = keras.layers.Dense(
+            nneur, activation=activ_surface, name='dense_lay'
+        )(lay_inp)
+
+    hidden1 = rnnlayer(nneur, return_sequences=True, go_backwards=False)(
+        lay_inp, initial_state=init_state
+    )
+    hidden2 = rnnlayer(nneur, return_sequences=True, go_backwards=True)(
+        hidden1
+    )
+
+    reverse_layer_object = keras.layers.Lambda(
+        lambda x: K.reverse(x, axes=1),
+        output_shape=hidden2.shape[1:]
+    )
+    hidden2 = reverse_layer_object(hidden2)
+    # hidden2 = K.reverse(hidden2, axes=1)
+
+    hidden2 = keras.layers.Concatenate(axis=2)([hidden1, hidden2])
+
+    flux_sw = keras.layers.Dense(2, activation=activ_last, name='sw_denorm')(
+        hidden2
+    )
+    flux_sw = keras.layers.Multiply(name='sw')([flux_sw, incflux])
+    hr_sw = HeatingRateLayer(name='hr_sw')([flux_sw, hl_p])
+
+    new_dimensions = hr_sw.shape[1:] + (1,)
+    hr_sw = keras.layers.Reshape(target_shape=new_dimensions)(hr_sw)
+    # hr_sw = K.expand_dims(hr_sw, axis=-1)
+
+    hr_sw = keras.layers.ZeroPadding1D(padding=(0, 1))(hr_sw)
+    outputs = keras.layers.Concatenate(axis=-1)([flux_sw, hr_sw])
+    model = Model(inputs=all_inp, outputs=outputs)
+    model.summary()
+
+    model.compile(
+        loss='mse',
+        optimizer=keras.optimizers.Nadam(),
+        metrics=['mean_squared_error', 'mean_absolute_error']
+    )
+
+    return model
---
+++ ml4rt/scripts/train_neural_net_for_peter.py
@@ -0,0 +1,240 @@
+"""Trains neural net for Peter Ukkonen."""
+
+import argparse
+from gewittergefahr.gg_utils import time_conversion
+from ml4rt.machine_learning import neural_net
+from ml4rt.machine_learning import peter_brnn_architecture
+
+TIME_FORMAT = '%Y-%m-%d-%H%M%S'
+
+TRAINING_DIR_ARG_NAME = 'input_training_dir_name'
+VALIDATION_DIR_ARG_NAME = 'input_validation_dir_name'
+OUTPUT_MODEL_DIR_ARG_NAME = 'output_model_dir_name'
+USE_GENERATOR_FOR_VALIDN_ARG_NAME = 'use_generator_for_validn'
+
+FIRST_TRAIN_TIME_ARG_NAME = 'first_training_time_string'
+LAST_TRAIN_TIME_ARG_NAME = 'last_training_time_string'
+FIRST_VALIDN_TIME_ARG_NAME = 'first_validn_time_string'
+LAST_VALIDN_TIME_ARG_NAME = 'last_validn_time_string'
+
+BATCH_SIZE_ARG_NAME = 'num_examples_per_batch'
+NUM_EPOCHS_ARG_NAME = 'num_epochs'
+NUM_TRAINING_BATCHES_ARG_NAME = 'num_training_batches_per_epoch'
+NUM_VALIDN_BATCHES_ARG_NAME = 'num_validn_batches_per_epoch'
+PLATEAU_LR_MULTIPLIER_ARG_NAME = 'plateau_lr_multiplier'
+EARLY_STOPPING_PATIENCE_ARG_NAME = 'early_stopping_patience_epochs'
+
+TRAINING_DIR_HELP_STRING = (
+    'Name of directory with training examples.  Files therein will be found by '
+    '`example_io.find_file` and read by `example_io.read_file`.'
+)
+VALIDATION_DIR_HELP_STRING = (
+    'Same as `{0:s}` but for validation (monitoring) examples.'
+).format(
+    TRAINING_DIR_ARG_NAME
+)
+OUTPUT_MODEL_DIR_HELP_STRING = (
+    'Name of output directory.  Model will be saved here.'
+)
+USE_GENERATOR_FOR_VALIDN_HELP_STRING = (
+    'Boolean flag.  If 1, will use generator for validation data.  If 0, will '
+    'load all validation data into memory at once.'
+)
+
+TRAIN_TIME_HELP_STRING = (
+    'Time (format "yyyy-mm-dd-HHMMSS").  The training period will be '
+    '`{0:s}`...`{1:s}`.'
+).format(
+    FIRST_TRAIN_TIME_ARG_NAME, LAST_TRAIN_TIME_ARG_NAME
+)
+VALIDN_TIME_HELP_STRING = (
+    'Time (format "yyyy-mm-dd-HHMMSS").  The validation (monitoring) period '
+    'will be `{0:s}`...`{1:s}`.'
+).format(
+    FIRST_VALIDN_TIME_ARG_NAME, LAST_VALIDN_TIME_ARG_NAME
+)
+
+BATCH_SIZE_HELP_STRING = (
+    'Number of examples in each training and validation (monitoring) batch.'
+)
+NUM_EPOCHS_HELP_STRING = 'Number of epochs.'
+NUM_TRAINING_BATCHES_HELP_STRING = 'Number of training batches per epoch.'
+NUM_VALIDN_BATCHES_HELP_STRING = (
+    'Number of validation (monitoring) batches per epoch.'
+)
+PLATEAU_LR_MULTIPLIER_HELP_STRING = (
+    'Multiplier for learning rate.  Learning rate will be multiplied by this '
+    'factor upon plateau in validation performance.'
+)
+EARLY_STOPPING_PATIENCE_HELP_STRING = (
+    'Patience for early stopping.  Early stopping will be triggered if '
+    'validation loss has not improved over this many epochs.'
+)
+
+INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + TRAINING_DIR_ARG_NAME, type=str, required=True,
+    help=TRAINING_DIR_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + VALIDATION_DIR_ARG_NAME, type=str, required=True,
+    help=VALIDATION_DIR_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_MODEL_DIR_ARG_NAME, type=str, required=True,
+    help=OUTPUT_MODEL_DIR_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + USE_GENERATOR_FOR_VALIDN_ARG_NAME, type=int, required=False,
+    default=1, help=USE_GENERATOR_FOR_VALIDN_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + FIRST_TRAIN_TIME_ARG_NAME, type=str, required=True,
+    help=TRAIN_TIME_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + LAST_TRAIN_TIME_ARG_NAME, type=str, required=True,
+    help=TRAIN_TIME_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + FIRST_VALIDN_TIME_ARG_NAME, type=str, required=True,
+    help=VALIDN_TIME_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + LAST_VALIDN_TIME_ARG_NAME, type=str, required=True,
+    help=VALIDN_TIME_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + BATCH_SIZE_ARG_NAME, type=int, required=True,
+    help=BATCH_SIZE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_EPOCHS_ARG_NAME, type=int, required=True,
+    help=NUM_EPOCHS_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_TRAINING_BATCHES_ARG_NAME, type=int, required=True,
+    help=NUM_TRAINING_BATCHES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_VALIDN_BATCHES_ARG_NAME, type=int, required=True,
+    help=NUM_VALIDN_BATCHES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + PLATEAU_LR_MULTIPLIER_ARG_NAME, type=float, required=True,
+    help=PLATEAU_LR_MULTIPLIER_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + EARLY_STOPPING_PATIENCE_ARG_NAME, type=int, required=True,
+    help=EARLY_STOPPING_PATIENCE_HELP_STRING
+)
+
+
+def _run(training_dir_name, validation_dir_name, output_model_dir_name,
+         use_generator_for_validn,
+         first_training_time_string, last_training_time_string,
+         first_validn_time_string, last_validn_time_string,
+         num_examples_per_batch, num_epochs,
+         num_training_batches_per_epoch, num_validn_batches_per_epoch,
+         plateau_lr_multiplier, early_stopping_patience_epochs):
+    """Trains neural net for Peter Ukkonen.
+
+    This is effectively the main method.
+
+    :param training_dir_name: See documentation at top of this script.
+    :param validation_dir_name: Same.
+    :param output_model_dir_name: Same.
+    :param use_generator_for_validn: Same.
+    :param first_training_time_string: Same.
+    :param last_training_time_string: Same.
+    :param first_validn_time_string: Same.
+    :param last_validn_time_string: Same.
+    :param num_examples_per_batch: Same.
+    :param num_epochs: Same.
+    :param num_training_batches_per_epoch: Same.
+    :param num_validn_batches_per_epoch: Same.
+    :param plateau_lr_multiplier: Same.
+    :param early_stopping_patience_epochs: Same.
+    """
+
+    first_training_time_unix_sec = time_conversion.string_to_unix_sec(
+        first_training_time_string, TIME_FORMAT
+    )
+    last_training_time_unix_sec = time_conversion.string_to_unix_sec(
+        last_training_time_string, TIME_FORMAT
+    )
+    first_validn_time_unix_sec = time_conversion.string_to_unix_sec(
+        first_validn_time_string, TIME_FORMAT
+    )
+    last_validn_time_unix_sec = time_conversion.string_to_unix_sec(
+        last_validn_time_string, TIME_FORMAT
+    )
+
+    training_option_dict = {
+        neural_net.EXAMPLE_DIRECTORY_KEY: training_dir_name,
+        neural_net.BATCH_SIZE_KEY: num_examples_per_batch,
+        neural_net.FIRST_TIME_KEY: first_training_time_unix_sec,
+        neural_net.LAST_TIME_KEY: last_training_time_unix_sec
+    }
+    validation_option_dict = {
+        neural_net.EXAMPLE_DIRECTORY_KEY: validation_dir_name,
+        neural_net.BATCH_SIZE_KEY: num_examples_per_batch,
+        neural_net.FIRST_TIME_KEY: first_validn_time_unix_sec,
+        neural_net.LAST_TIME_KEY: last_validn_time_unix_sec
+    }
+
+    model_object = peter_brnn_architecture.rnn_sw()
+
+    neural_net.train_model_with_generator_for_peter(
+        model_object=model_object,
+        output_dir_name=output_model_dir_name,
+        num_epochs=num_epochs,
+        training_option_dict=training_option_dict,
+        validation_option_dict=validation_option_dict,
+        use_generator_for_validn=use_generator_for_validn,
+        num_training_batches_per_epoch=num_training_batches_per_epoch,
+        num_validation_batches_per_epoch=num_validn_batches_per_epoch,
+        plateau_lr_multiplier=plateau_lr_multiplier,
+        early_stopping_patience_epochs=early_stopping_patience_epochs,
+    )
+
+
+if __name__ == '__main__':
+    INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
+
+    _run(
+        training_dir_name=getattr(INPUT_ARG_OBJECT, TRAINING_DIR_ARG_NAME),
+        validation_dir_name=getattr(INPUT_ARG_OBJECT, VALIDATION_DIR_ARG_NAME),
+        output_model_dir_name=getattr(
+            INPUT_ARG_OBJECT, OUTPUT_MODEL_DIR_ARG_NAME
+        ),
+        use_generator_for_validn=bool(getattr(
+            INPUT_ARG_OBJECT, USE_GENERATOR_FOR_VALIDN_ARG_NAME
+        )),
+        first_training_time_string=getattr(
+            INPUT_ARG_OBJECT, FIRST_TRAIN_TIME_ARG_NAME
+        ),
+        last_training_time_string=getattr(
+            INPUT_ARG_OBJECT, LAST_TRAIN_TIME_ARG_NAME
+        ),
+        first_validn_time_string=getattr(
+            INPUT_ARG_OBJECT, FIRST_VALIDN_TIME_ARG_NAME
+        ),
+        last_validn_time_string=getattr(
+            INPUT_ARG_OBJECT, LAST_VALIDN_TIME_ARG_NAME
+        ),
+        num_examples_per_batch=getattr(INPUT_ARG_OBJECT, BATCH_SIZE_ARG_NAME),
+        num_epochs=getattr(INPUT_ARG_OBJECT, NUM_EPOCHS_ARG_NAME),
+        num_training_batches_per_epoch=getattr(
+            INPUT_ARG_OBJECT, NUM_TRAINING_BATCHES_ARG_NAME
+        ),
+        num_validn_batches_per_epoch=getattr(
+            INPUT_ARG_OBJECT, NUM_VALIDN_BATCHES_ARG_NAME
+        ),
+        plateau_lr_multiplier=getattr(
+            INPUT_ARG_OBJECT, PLATEAU_LR_MULTIPLIER_ARG_NAME
+        ),
+        early_stopping_patience_epochs=getattr(
+            INPUT_ARG_OBJECT, EARLY_STOPPING_PATIENCE_ARG_NAME
+        )
+    )
