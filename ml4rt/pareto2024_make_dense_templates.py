677e29e3a9129217bc605dd9544b3851e27c56fc
thunderhoser <lagerqui@ualberta.ca>
2024-06-19 12:04:32 -0600

    FOOg

--- ml4rt/pareto2024_make_dense_templates.py
+++ ml4rt/pareto2024_make_dense_templates.py
@@ -61,8 +61,9 @@ DEFAULT_OPTION_DICT = {
 }
 
 DUMMY_GENERATOR_OPTION_DICT = {
-    neural_net.VECTOR_TARGET_NORM_TYPE_KEY: None,
-    neural_net.SCALAR_TARGET_NORM_TYPE_KEY: None
+    neural_net.NORMALIZE_PREDICTORS_KEY: True,
+    neural_net.NORMALIZE_VECTOR_TARGETS_KEY: False,
+    neural_net.NORMALIZE_SCALAR_TARGETS_KEY: False
 }
 
 
@@ -133,14 +134,15 @@ def _run():
                 num_validation_batches_per_epoch=100,
                 validation_option_dict=DUMMY_GENERATOR_OPTION_DICT,
                 loss_function_or_dict=LOSS_DICT,
-                do_early_stopping=True,
+                early_stopping_patience_epochs=200,
                 plateau_lr_multiplier=0.6,
                 dense_architecture_dict=option_dict,
                 cnn_architecture_dict=None,
                 bnn_architecture_dict=None,
                 u_net_architecture_dict=None,
                 u_net_plusplus_architecture_dict=None,
-                u_net_3plus_architecture_dict=None
+                u_net_3plus_architecture_dict=None,
+                use_ryan_architecture=False
             )
 
 
