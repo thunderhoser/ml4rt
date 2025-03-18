761abf4245ccd70c9461d54f42c05fb151c878d1
thunderhoser <lagerqui@ualberta.ca>
2025-03-17 20:38:34 -0600

    FOO

--- ml4rt/isotonic_regression.py
+++ ml4rt/isotonic_regression.py
@@ -503,6 +503,7 @@ class IsotonicRegressionLayerMemoryHeavy(keras.layers.Layer):
             dtype=tensorflow.int32
         )
         index_tensor = tensorflow.argmax(mask_tensor, axis=-1)
+        index_tensor = tensorflow.cast(index_tensor, dtype=tensorflow.int32)
         index_tensor = self.max_num_thresholds - 1 - index_tensor
         index_tensor = index_tensor + 1
 
