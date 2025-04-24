fe90bfd9c9945f543f34d2b33e58b8ea5804be10
thunderhoser <lagerqui@ualberta.ca>
2025-04-24 13:32:18 -0600

    FOO

--- ml4rt/convnext.py
+++ ml4rt/convnext.py
@@ -17,7 +17,7 @@ EXPANSION_FACTOR_FOR_CONVNEXT = 4
 INIT_VALUE_FOR_LAYER_SCALE = 1e-6
 
 
-@keras.saving.register_keras_serializable()
+# @keras.saving.register_keras_serializable()
 class LayerScale(keras.layers.Layer):
     """Layer-scale module.
 
@@ -52,7 +52,7 @@ class LayerScale(keras.layers.Layer):
         return config
 
 
-@keras.saving.register_keras_serializable()
+# @keras.saving.register_keras_serializable()
 class GRN(keras.layers.Layer):
     """Global response normalization.
 
@@ -113,7 +113,7 @@ class GRN(keras.layers.Layer):
         return config
 
 
-@keras.saving.register_keras_serializable()
+# @keras.saving.register_keras_serializable()
 class StochasticDepth(keras.layers.Layer):
     def __init__(self, survival_prob=0.9, **kwargs):
         super().__init__(**kwargs)
