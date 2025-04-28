c42e15ee44b32760a6e1a8c2aa55f081fa5d5f36
thunderhoser <lagerqui@ualberta.ca>
2025-04-28 11:53:03 -0600

    FOO

--- ml4rt/slurm/shortwave_bnn_crps_experiment00/validation/apply_neural_net.sh
+++ ml4rt/slurm/shortwave_bnn_crps_experiment00/validation/apply_neural_net.sh
@@ -3,6 +3,9 @@
 # set -x
 
 model_dir_name=$1
+prediction_file_index=$2
+prediction_file_index="$((prediction_file_index-1))"
+
 model_file_name="${model_dir_name}/model.h5"
 model_dir_name="${model_dir_name}/model"
 
@@ -13,17 +16,15 @@ FIRST_EVAL_TIME_STRINGS=("2020-01-02-000000" "2020-02-12-000000" "2020-03-24-000
 LAST_EVAL_TIME_STRINGS=("2020-01-15-235959" "2020-02-25-235959" "2020-04-06-235959" "2020-05-18-235959" "2020-06-29-235959" "2020-08-09-235959" "2020-09-19-235959" "2020-11-02-235959" "2020-12-13-235959")
 PATHLESS_OUTPUT_FILE_NAMES=("predictions_part01.nc" "predictions_part02.nc" "predictions_part03.nc" "predictions_part04.nc" "predictions_part05.nc" "predictions_part06.nc" "predictions_part07.nc" "predictions_part08.nc" "predictions_part09.nc")
 
-for i in ${!FIRST_EVAL_TIME_STRINGS[@]}; do
-    first_eval_time_string=${FIRST_EVAL_TIME_STRINGS[$i]}
-    last_eval_time_string=${LAST_EVAL_TIME_STRINGS[$i]}
-    prediction_file_name="${model_dir_name}/validation_perturbed_for_uq/${PATHLESS_OUTPUT_FILE_NAMES[$i]}"
-    
-    python3 -u "${CODE_DIR_NAME}/apply_neural_net.py" \
-    --input_model_file_name="${model_file_name}" \
-    --input_example_dir_name="${EXAMPLE_DIR_NAME}" \
-    --first_time_string="${first_eval_time_string}" \
-    --last_time_string="${last_eval_time_string}" \
-    --num_bnn_iterations=10 \
-    --max_ensemble_size=50 \
-    --output_file_name="${prediction_file_name}"
-done
+first_eval_time_string=${FIRST_EVAL_TIME_STRINGS[$prediction_file_index]}
+last_eval_time_string=${LAST_EVAL_TIME_STRINGS[$prediction_file_index]}
+prediction_file_name="${model_dir_name}/validation_perturbed_for_uq/${PATHLESS_OUTPUT_FILE_NAMES[$prediction_file_index]}"
+
+python3 -u "${CODE_DIR_NAME}/apply_neural_net.py" \
+--input_model_file_name="${model_file_name}" \
+--input_example_dir_name="${EXAMPLE_DIR_NAME}" \
+--first_time_string="${first_eval_time_string}" \
+--last_time_string="${last_eval_time_string}" \
+--num_bnn_iterations=10 \
+--max_ensemble_size=50 \
+--output_file_name="${prediction_file_name}"
